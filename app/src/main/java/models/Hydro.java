e39ee6c00143cec4f965a19536918ffadc4cfc7b
MANBEERGIL <manbeergill545@gmail.com>
2020-04-14 19:31:17 -0400

    creating constructer

--- app/src/main/java/models/Hydro.java
+++ app/src/main/java/models/Hydro.java
@@ -1,4 +1,27 @@
 package models;
 
 public class Hydro {
+    private  String agencyName;
+    private String unitConsumed;
+
+    public Hydro(String agencyName, String unitConsumed) {
+        this.agencyName = agencyName;
+        this.unitConsumed = unitConsumed;
+    }
+
+    public String getAgencyName() {
+        return agencyName;
+    }
+
+    public void setAgencyName(String agencyName) {
+        this.agencyName = agencyName;
+    }
+
+    public String getUnitConsumed() {
+        return unitConsumed;
+    }
+
+    public void setUnitConsumed(String unitConsumed) {
+        this.unitConsumed = unitConsumed;
+    }
 }
--- app/src/main/java/models/Internet.java
+++ app/src/main/java/models/Internet.java
@@ -4,5 +4,24 @@ public class Internet {
     private  String providerName;
     private  String internetGbUsed;
 
+    public Internet(String providerName, String internetGbUsed) {
+        this.providerName = providerName;
+        this.internetGbUsed = internetGbUsed;
+    }
 
+    public String getProviderName() {
+        return providerName;
+    }
+
+    public void setProviderName(String providerName) {
+        this.providerName = providerName;
+    }
+
+    public String getInternetGbUsed() {
+        return internetGbUsed;
+    }
+
+    public void setInternetGbUsed(String internetGbUsed) {
+        this.internetGbUsed = internetGbUsed;
+    }
 }
