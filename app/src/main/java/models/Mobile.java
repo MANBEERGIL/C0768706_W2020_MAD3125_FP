55b310b8dcc05ace0f4eeeed8452e271cc250191
MANBEERGIL <manbeergill545@gmail.com>
2020-04-14 19:32:34 -0400

    generating getter and setter

--- app/src/main/java/models/Mobile.java
+++ app/src/main/java/models/Mobile.java
@@ -12,4 +12,36 @@ public class Mobile {
         this.mobileNumber = mobileNumber;
         this.internetGbUsed = internetGbUsed;
     }
+
+    public String getMobileManufacturerName() {
+        return mobileManufacturerName;
+    }
+
+    public void setMobileManufacturerName(String mobileManufacturerName) {
+        this.mobileManufacturerName = mobileManufacturerName;
+    }
+
+    public String getPlanName() {
+        return planName;
+    }
+
+    public void setPlanName(String planName) {
+        this.planName = planName;
+    }
+
+    public int getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(int mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public int getInternetGbUsed() {
+        return internetGbUsed;
+    }
+
+    public void setInternetGbUsed(int internetGbUsed) {
+        this.internetGbUsed = internetGbUsed;
+    }
 }
