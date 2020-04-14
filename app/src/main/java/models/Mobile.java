1853402f38e0134acadd07f7d687b9a718ffe1e0
MANBEERGIL <manbeergill545@gmail.com>
2020-04-14 19:31:53 -0400

    creating constructer in mobile class

--- app/src/main/java/models/Mobile.java
+++ app/src/main/java/models/Mobile.java
@@ -5,4 +5,11 @@ public class Mobile {
     private  String planName;
     private int mobileNumber;
     private int internetGbUsed;
+
+    public Mobile(String mobileManufacturerName, String planName, int mobileNumber, int internetGbUsed) {
+        this.mobileManufacturerName = mobileManufacturerName;
+        this.planName = planName;
+        this.mobileNumber = mobileNumber;
+        this.internetGbUsed = internetGbUsed;
+    }
 }
